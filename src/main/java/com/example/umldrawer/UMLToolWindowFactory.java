79ed422ca7f864080a2a04e4b32b3546f02c7757
rnaryshkin <r.naryshkin99@gmail.com>
2023-03-15 22:26:48 +0300

    javaFX with a few ways of using

--- src/main/java/com/example/umldrawer/UMLToolWindowFactory.java
+++ src/main/java/com/example/umldrawer/UMLToolWindowFactory.java
@@ -3,6 +3,8 @@ package com.example.umldrawer;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Group;
@@ -27,22 +29,73 @@ public class UMLToolWindowFactory implements ToolWindowFactory {
      * @param toolWindow current tool windowA
      */
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        final JFXPanel fxPanel = new JFXPanel();
+//        final JFXPanel fxPanel = new JFXPanel();
+//        JComponent component = toolWindow.getComponent();
+//
+//        Platform.setImplicitExit(false);
+//        Platform.runLater(() -> {
+//            WebView webView = new WebView();
+//            WebEngine webEngine = webView.getEngine();
+//            webEngine.load("file:///C:\\Users\\rnary\\IdeaProjects\\cpppainter\\generated-html5\\CppSampleModel\\index.html");
+//            webEngine.load("file:///C:\\Users\\rnary\\IdeaProjects\\UMLDrawer\\src\\main\\java\\com\\example\\generated-html5\\CppSampleModel\\index.html");
+//            Group root  =  new Group();
+//            Scene scene  =  new  Scene(root, 40, 100);
+//            root.getChildren().add(webView);
+//
+//            fxPanel.setScene(scene);
+//        });
+
+//        component.getParent().add(fxPanel);
+        JRootPane jRootPane = new JRootPane();
+
+        JPanel myToolWindowContent = new JPanel();
+        myToolWindowContent.setAutoscrolls(true);
+
+        JFrame frame = new JFrame();
+//        final JFXPanel fxPanel = new JFXPanel();
         JComponent component = toolWindow.getComponent();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTabbedPane jtp = new JTabbedPane();
+        jtp.add("One", createTab("http://www.example.com"));
+        jtp.add("Two", createTab("http://www.example.net"));
+        jtp.add("Three", createTab("http://www.example.org"));
+        frame.add(jtp, BorderLayout.CENTER);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
 
-        Platform.setImplicitExit(false);
+        myToolWindowContent.add(createTab("file:///C:\\Users\\rnary\\IdeaProjects\\cpppainter\\generated-html5\\CppSampleModel\\index.html"));
+        myToolWindowContent.add(createTab("http://www.example.com"));
+        myToolWindowContent.add(jtp, BorderLayout.CENTER);
+        component.getParent().add(myToolWindowContent);
+
+//        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+//        Content content = contentFactory.createContent(myToolWindowContent, "", true);
+//        toolWindow.getContentManager().addContent(content);
+    }
+
+    private JFXPanel createTab(String s) {
+        final JFXPanel fxPanel = new JFXPanel() {
+
+            @Override
+            public Dimension getPreferredSize() {
+                return new Dimension(640, 480);
+            }
+        };
         Platform.runLater(() -> {
-            WebView webView = new WebView();
-            WebEngine webEngine = webView.getEngine();
-            webEngine.load("file:///C:\\Users\\rnary\\IdeaProjects\\cpppainter\\generated-html5\\CppSampleModel\\index.html");
-            webEngine.load("file:///C:\\Users\\rnary\\IdeaProjects\\UMLDrawer\\src\\main\\java\\com\\example\\generated-html5\\CppSampleModel\\index.html");
-            Group root  =  new Group();
-            Scene scene  =  new  Scene(root, 40, 100);
-            root.getChildren().add(webView);
-
-            fxPanel.setScene(scene);
+            initFX(fxPanel, s);
         });
+        return fxPanel;
+    }
 
-        component.getParent().add(fxPanel);
+    private void initFX(JFXPanel fxPanel, String s) {
+        // This method is invoked on the JavaFX thread
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root);
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        webEngine.load(s);
+        root.getChildren().add(webView);
+        fxPanel.setScene(scene);
     }
 }
