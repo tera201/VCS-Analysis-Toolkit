b82e5bcf52c8693e5296767ad0185e3a85b6ecd5
rnaryshkin <r.naryshkin99@gmail.com>
2023-04-07 04:44:25 +0300

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.5.2"
+    id("org.jetbrains.intellij") version "1.13.3"
 }
 
 group = "com.example"
@@ -28,7 +28,7 @@ repositories {
 dependencies {
     implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
     implementation("org.example:javafx-uml-city-builder:latest.integration")
-    implementation("org.example:javafx-uml-graph:latest.integration")
+    implementation("org.example:javafx-uml-graph-idea:latest.integration")
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
