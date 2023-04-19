649a383f93308e595a34b823b673e49ab1fc9b0f
rnaryshkin <r.naryshkin99@gmail.com>
2023-04-19 05:38:59 +0300

    add kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
 }
 
 group = "com.example"
@@ -15,31 +16,23 @@ repositories {
             username = project.properties["username"].toString()
             password = project.properties["password"].toString()
         }
-        }
-    maven {
-        url = uri("https://maven.pkg.github.com/tera201/JavaFXUMLGraph")
-        credentials {
-            username = project.properties["username"].toString()
-            password = project.properties["password"].toString()
-        }
-
     }
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
     implementation("org.example:javafx-uml-city-builder:latest.integration")
     implementation("org.example:javafx-uml-graph-idea:1.1-SNAPSHOT")
-    implementation("org.example:cpp-to-uml:1.1-SNAPSHOT")
+    implementation("org.example:cpp-to-uml:1.2-SNAPSHOT")
     implementation("org.example:swrminer:1.1-SNAPSHOT")
-    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
     version.set("2023.1")
     type.set("IC") // Target IDE Platform
-
     plugins.set(listOf("com.intellij.javafx:1.0.4"))
 }
 
@@ -50,18 +43,29 @@ tasks {
         targetCompatibility = "11"
     }
 
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+            apiVersion = "1.8"
+            languageVersion = "1.8"
+        }
+    }
+
     patchPluginXml {
         sinceBuild.set("203")
         untilBuild.set("231.*")
     }
+}
 
-//    signPlugin {
-//        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-//        privateKey.set(System.getenv("PRIVATE_KEY"))
-//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-//    }
-//
-//    publishPlugin {
-//        token.set(System.getenv("PUBLISH_TOKEN"))
-//    }
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.languageVersion = "1.8"
+        kotlinOptions.apiVersion = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.languageVersion = "1.8"
+        kotlinOptions.apiVersion = "1.8"
+    }
 }
