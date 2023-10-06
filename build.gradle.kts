9d82169a72e573d4af51039db0a78067441b11f9
rnaryshkin <r.naryshkin99@gmail.com>
2023-10-06 16:42:01 +0300

    refactor dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
     implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
-    implementation("org.example:javafx-uml-city-builder:latest.integration")
+    implementation("org.tera201:javafx-uml-city-builder:latest.integration")
     implementation("org.example:javafx-uml-graph-idea:0.0.1-SNAPSHOT")
     implementation("org.example:code-to-uml:0.0.2-SNAPSHOT")
     implementation("org.example:swrminer:0.0.2-SNAPSHOT")
