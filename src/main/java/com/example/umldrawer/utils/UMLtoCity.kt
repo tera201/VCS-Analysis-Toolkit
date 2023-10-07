d9ed321e9939aa0d81ff36710f7890a0f38862d2
rnaryshkin <r.naryshkin99@gmail.com>
2023-10-07 21:07:45 +0300

    update

--- src/main/java/com/example/umldrawer/utils/UMLtoCity.kt
+++ src/main/java/com/example/umldrawer/utils/UMLtoCity.kt
@@ -1,6 +1,7 @@
 package com.example.umldrawer.utils
 
 import org.eclipse.uml2.uml.*
+import org.example.elements.Building
 import org.example.elements.City
 import org.example.elements.Quarter
 
@@ -10,6 +11,7 @@ fun Package.toCity(city: City) {
         .forEach {
             when (it) {
                 is Package -> it.generatePackage(city)
+                is Class -> it.generateClass(null)
             }
         }
 }
@@ -22,6 +24,31 @@ private fun Package.generatePackage(city: City) {
         .forEach {
             when (it) {
                 is Package -> it.generatePackage(city)
+                is Class -> it.generateClass(quarter)
+                is Interface -> it.generateInterface(quarter)
+                is Enumeration -> it.generateEnumeration(quarter)
             }
         }
+}
+
+private fun Class.generateClass(quarter: Quarter?) {
+    val size = ownedComments[0].body.toDouble()
+    val methods = ownedComments[1].body.toDouble()
+    val side = size / 20
+    println("$name $size $methods ${size::class.java}")
+    val building = Building(name, side, 10 * methods, side)
+    quarter?.addBuilding(building)
+}
+
+private fun Interface.generateInterface(quarter: Quarter?) {
+    val size = ownedComments[0].body.toDouble()
+    val methods = ownedComments[1].body.toDouble()
+    val side = size / 20
+    val building = Building(name, side, 10 * methods, side)
+    quarter?.addBuilding(building)
+}
+
+private fun Enumeration.generateEnumeration(quarter: Quarter?) {
+    val building = Building(name, 100.0, 900.0, 100.0)
+    quarter?.addBuilding(building)
 }
