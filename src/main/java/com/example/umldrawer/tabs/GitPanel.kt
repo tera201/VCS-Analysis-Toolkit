8f756b433b0114bf4ce01d544b70a7b655614602
rnaryshkin <r.naryshkin99@gmail.com>
2023-10-14 22:45:19 +0300

    add double click checkout update file view add open file by double click

--- src/main/java/com/example/umldrawer/tabs/GitPanel.kt
+++ src/main/java/com/example/umldrawer/tabs/GitPanel.kt
@@ -5,11 +5,17 @@ import com.example.umldrawer.utils.toCity
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileChooser.FileChooser
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.observable.util.whenSizeChanged
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.ui.ColorUtil
+import com.intellij.ui.ColoredTreeCellRenderer
 import com.intellij.ui.components.JBList
 import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.treeStructure.Tree
+import com.intellij.util.PlatformIcons
 import com.intellij.util.ui.UIUtil
 import java20.console.JavaParserRunner
 import javafx.application.Platform
@@ -20,17 +26,21 @@ import uml.util.UMLModelHandler
 import java.awt.Dimension
 import java.awt.FlowLayout
 import java.awt.Graphics
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import java.io.File
 import java.io.IOException
 import javax.swing.*
 import javax.swing.plaf.basic.BasicSplitPaneDivider
 import javax.swing.plaf.basic.BasicSplitPaneUI
 import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeModel
 import kotlin.concurrent.thread
 
 class GitPanel : JPanel() {
     private var settings:UMLToolkitSettings = UMLToolkitSettings.getInstance()
     private var myRepo: SCMRepository? = null
+    val buildModel = BuildModel()
     private val jTextArea = JTextArea()
     companion object {
         var model: Model? = null
@@ -41,9 +51,10 @@ class GitPanel : JPanel() {
     val tagList = JBList<String>()
     val branchListScrollPane = JBScrollPane(branchList)
     val tagListScrollPane = JBScrollPane(tagList)
+    val pathJTree = Tree()
     private val vcSplitPane = JSplitPane(JSplitPane.VERTICAL_SPLIT, branchListScrollPane, tagListScrollPane)
     private val filesTreeJBScrollPane =
-        JBScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER)
+        JBScrollPane(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)
     private val showSplitPane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT, filesTreeJBScrollPane, vcSplitPane)
     private val projectCache = "${System.getProperty("user.dir")}/UmlToolkitCache/"
     private val modelCache = "${System.getProperty("user.dir")}/UmlToolkitCache/Models"
@@ -95,6 +106,7 @@ class GitPanel : JPanel() {
         getButton.addActionListener {
             clickGet(urlField)
         }
+        setupTree()
         configureSplitPanes()
         this.add(urlField)
         this.add(getButton)
@@ -116,10 +128,10 @@ class GitPanel : JPanel() {
                 vcSplitPane.dividerLocation = it.height / 2
         }
         setupListSelectionListeners()
+        setupListDoubleClickAction()
     }
 
     private fun setupListSelectionListeners() {
-
         branchList.addListSelectionListener {
             if (!it!!.valueIsAdjusting && !isClearingSelection && tagList.selectedValue != null) {
                 isClearingSelection = true
@@ -135,6 +147,39 @@ class GitPanel : JPanel() {
                 isClearingSelection = false
             }
         }
+    }
+
+    private fun setupListDoubleClickAction() {
+        val branchMouseEvent = object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e!!.clickCount == 2) {
+                    val index = branchList.locationToIndex(e.point)
+                    if (index >= 0) {
+                        val selectedItem = branchList.model.getElementAt(index)
+                        onListItemDoubleClicked(selectedItem)
+                    }
+                }
+            }
+        }
+        val tagMouseEvent = object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e!!.clickCount == 2) {
+                    val index = tagList.locationToIndex(e.point)
+                    if (index >= 0) {
+                        val selectedItem = tagList.model.getElementAt(index)
+                        onListItemDoubleClicked(selectedItem)
+                    }
+                }
+            }
+        }
+        branchList.addMouseListener(branchMouseEvent)
+        tagList.addMouseListener(tagMouseEvent)
+    }
+
+    private fun onListItemDoubleClicked(item: String) {
+        println("Double clicked on item: $item")
+        buildModel.checkout(myRepo, item)
+        updatePathPanel()
     }
 
     private fun populateJBList(targetPane:JBList<String>, stringList: List<String>) {
@@ -169,6 +214,7 @@ class GitPanel : JPanel() {
         val modelControlPanel = JPanel()
 
         val analyzeButton = JButton("Analyze")
+        val analyzeAllButton = JButton("AnalyzeAll")
         val saveUmlFileButton = JButton("Save UML model")
         val getUmlFileButton = JButton("Get UML model")
 
@@ -235,7 +281,6 @@ class GitPanel : JPanel() {
     private fun clickGet(textField: JTextField) {
         //TODO: add regex
         if (textField.text != "") {
-            val buildModel = BuildModel()
             val directoryPath = "$projectCache/${buildModel.getRepoNameByUrl(textField.text)}"
             val directory = File(directoryPath)
             if (directory.exists() && directory.isDirectory) {
@@ -253,24 +298,69 @@ class GitPanel : JPanel() {
     }
 
     private fun updatePathPanel() {
-        val root = DefaultMutableTreeNode(myRepo!!.path.split("/").last())
-        val directories = File(myRepo!!.path).list { dir, name -> File(dir, name).isDirectory }
-        buildTree(directories, root, myRepo!!.path)
-        filesTreeJBScrollPane.setViewportView(JTree(root))
+        val root = DefaultMutableTreeNode(myRepo!!.path)
+        buildTree(File(myRepo!!.path), root)
+        pathJTree.model = DefaultTreeModel(root)
+        filesTreeJBScrollPane.setViewportView(pathJTree)
         filesTreeJBScrollPane.updateUI()
     }
 
-    private fun buildTree(directories: Array<String>?, rootTree: DefaultMutableTreeNode, rootPath: String) {
-        if (directories != null)
-            for (dir in directories) {
-                val node = DefaultMutableTreeNode(dir)
-                rootTree.add(node)
-                val nodeRootPath = "$rootPath/$dir"
-                val nodeFiles = File(nodeRootPath).list { dir, name -> File(dir, name).isFile }
-                if (nodeFiles != null) for (nodeFile in nodeFiles) node.add(DefaultMutableTreeNode(nodeFile))
-                val nodeDirectories = File(nodeRootPath).list { dir, name -> File(dir, name).isDirectory }
-                buildTree(nodeDirectories, node, nodeRootPath)
+    private fun buildTree(file: File, rootTree: DefaultMutableTreeNode) {
+        file.list { dir, name -> File(dir, name).isDirectory }?.forEach {
+            val path = "${file.absolutePath}/$it"
+            val node = DefaultMutableTreeNode(path)
+            rootTree.add(node)
+            buildTree(File(path), node)
+        }
+        file.list { dir, name -> File(dir, name).isFile }?.forEach {
+            val node = DefaultMutableTreeNode("${file.absolutePath}/$it")
+            rootTree.add(node)
+        }
+    }
+
+
+    private fun setupTree() {
+        pathJTree.cellRenderer = object : ColoredTreeCellRenderer() {
+            override fun customizeCellRenderer(
+                tree: JTree,
+                value: Any?,
+                selected: Boolean,
+                expanded: Boolean,
+                leaf: Boolean,
+                row: Int,
+                hasFocus: Boolean
+            ) {
+                val node = value as? DefaultMutableTreeNode ?: return
+                val file = File(node.userObject.toString())
+                if (file.isDirectory) {
+                    icon = PlatformIcons.FOLDER_ICON
+                } else
+                    icon = FileTypeManager.getInstance().getFileTypeByFileName(file.name).icon
+                append(file.name)
             }
+        }
+
+        pathJTree.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                if (e!!.clickCount == 2) {
+                    val selRow = pathJTree.getRowForLocation(e.x, e.y)
+                    if (selRow != -1) {
+                        val selPath = pathJTree.getPathForLocation(e.x, e.y)
+                        val selectedNode = selPath?.lastPathComponent as? DefaultMutableTreeNode
+                        onTreeNodeDoubleClicked(selectedNode)
+                    }
+                }
+            }
+        })
+    }
+}
+
+private fun onTreeNodeDoubleClicked(node: DefaultMutableTreeNode?) {
+    val filePath = node?.userObject.toString()
+    val virtualFile = LocalFileSystem.getInstance().findFileByPath(filePath)
+    if (virtualFile != null) {
+        ProjectManager.getInstance().openProjects.firstOrNull()
+            ?.let { FileEditorManager.getInstance(it).openFile(virtualFile, true) }
     }
 }
 
