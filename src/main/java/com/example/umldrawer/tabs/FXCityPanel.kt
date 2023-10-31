26672597b33b3faa4ec166d7f811527d9b074493
rnaryshkin <r.naryshkin99@gmail.com>
2023-10-31 16:50:03 +0000

    add combobox for models

--- src/main/java/com/example/umldrawer/tabs/FXCityPanel.kt
+++ src/main/java/com/example/umldrawer/tabs/FXCityPanel.kt
@@ -1,10 +1,13 @@
 package com.example.umldrawer.tabs
 
+import com.example.umldrawer.utils.toCity
+import com.intellij.openapi.ui.ComboBox
 import javafx.application.Platform
 import javafx.embed.swing.JFXPanel
 import javafx.scene.Scene
 import javafx.scene.SceneAntialiasing
 import javafx.scene.layout.StackPane
+import org.eclipse.uml2.uml.Model
 import org.tera201.InfoPane
 import org.tera201.MainSubScene
 import org.tera201.SelectionManager
@@ -12,20 +15,47 @@ import org.tera201.elements.FXSpace
 import org.tera201.elements.city.Building
 import org.tera201.elements.city.City
 import org.tera201.elements.city.Quarter
+import java.awt.BorderLayout
+import java.awt.FlowLayout
 import java.util.*
+import javax.swing.JPanel
+import kotlin.jvm.optionals.getOrNull
 
 
-class FXCityPanel : JFXPanel() {
+class FXCityPanel : JPanel() {
 
     companion object {
         var citySpace = FXSpace<Quarter>()
     }
+    val cityPane = object : JFXPanel() {}
     var rand = Random()
     private val SCENE_WIDTH = 800.0
     private val SCENE_HEIGHT = 600.0
+    private val modelComboBox = ComboBox(GitPanel.modelListContent)
+    private var model: Model? = null
+    private val topPanel = JPanel()
 
     init {
+        this.layout = BorderLayout()
+        topPanel.layout = FlowLayout(FlowLayout.LEFT)
+        topPanel.add(modelComboBox)
         Platform.runLater { initFXCity(citySpace) }
+        this.add(topPanel, BorderLayout.NORTH)
+        this.add(cityPane, BorderLayout.CENTER)
+
+        modelComboBox.addActionListener {
+            if (modelComboBox.selectedItem != null) {
+                val selectedModelName = modelComboBox.selectedItem as String
+                model = GitPanel.models.stream().filter { it.name == selectedModelName }.findAny().getOrNull()
+                Platform.runLater {
+                if (model != null) {
+                        citySpace.clean()
+                        model!!.toCity()
+                        citySpace.mainObject.updateView()
+                    }
+                }
+            }
+        }
 
 //         TODO: remove this listener
 //        this.addComponentListener(object : ComponentAdapter() {
@@ -55,7 +85,7 @@ class FXCityPanel : JFXPanel() {
         mainSubScene.widthProperty().bind(stackPane.widthProperty())
 
         val scene2 = Scene(stackPane, sceneWidth, sceneHeight, true, SceneAntialiasing.BALANCED)
-        this.scene = scene2
+        cityPane.scene = scene2
         city.updateView()
     }
 
