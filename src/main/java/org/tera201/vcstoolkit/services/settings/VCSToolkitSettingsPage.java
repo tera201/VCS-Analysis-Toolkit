bfd389da9890e4d16957285b2c377eec26bee10b
rnaryshkin <r.naryshkin99@gmail.com>
2023-11-04 16:55:34 +0000

    remove unused modes

--- src/main/java/org/tera201/vcstoolkit/services/settings/VCSToolkitSettingsPage.java
+++ src/main/java/org/tera201/vcstoolkit/services/settings/VCSToolkitSettingsPage.java
@@ -40,8 +40,8 @@ public class VCSToolkitSettingsPage implements Configurable {
         Hashtable<Integer, JLabel> labels = new Hashtable<>();
         labels.put(0, new JLabel("Unsafe"));
         labels.put(1, new JLabel("Safe"));
-        labels.put(2, new JLabel("Deep copy"));
-        labels.put(3, new JLabel("Commit"));
+//        labels.put(2, new JLabel("Deep copy"));
+//        labels.put(3, new JLabel("Commit"));
 
         slider1.setLabelTable(labels);
     }
