56aaf26f695954fdf01e7e4a7ba7d796f2761644
rnaryshkin <r.naryshkin99@gmail.com>
2023-11-10 23:54:22 +0000

    update lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.15.0"
+    id("org.jetbrains.intellij") version "1.16.0"
     id("org.jetbrains.kotlin.jvm") version "1.8.10"
     id("org.openjfx.javafxplugin") version "0.0.14"
 }
