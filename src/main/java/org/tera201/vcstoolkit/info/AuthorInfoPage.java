8960d79b670d0e80a3735a2ce57e652300a57863
rnaryshkin <r.naryshkin99@gmail.com>
2024-05-10 14:41:42 +0300

    updated AuthorInfoPage

--- src/main/java/org/tera201/vcstoolkit/info/AuthorInfoPage.java
+++ src/main/java/org/tera201/vcstoolkit/info/AuthorInfoPage.java
@@ -1,4 +1,14 @@
 package org.tera201.vcstoolkit.info;
 
+import javax.swing.*;
+
 public class AuthorInfoPage {
+    private JLabel authorNameLabel;
+    private JLabel commitCountLabel;
+    private JLabel commitFrequencyLabel;
+    private JLabel avgCommitTimeLabel;
+    private JLabel createdBranchesLabel;
+    private JLabel ownerPercentageLabel;
+    private JPanel filePiePanel;
+    private JPanel linesPiePanel;
 }
