dedbbe283a82129373ca63fda250a1fceecb7118
rnaryshkin <r.naryshkin99@gmail.com>
2025-02-08 23:08:58 +0000

    fix code-to-uml submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,11 @@
 rootProject.name = "IdeaPlugin"
 
-includeBuild("code-to-uml") {
-    dependencySubstitution {
-        substitute(module("org.tera201:code-to-uml"))
-            .using(project(":"))
-    }
-}
+//includeBuild("code-to-uml") {
+//    dependencySubstitution {
+//        substitute(module("org.tera201:code-to-uml"))
+//            .using(project(":"))
+//    }
+//}
+
+
+includeBuild("code-to-uml")
