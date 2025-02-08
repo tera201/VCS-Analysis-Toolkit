420a3f0f7a58aa9d78ad8cdcafac96a8990750c4
rnaryshkin <r.naryshkin99@gmail.com>
2025-02-08 23:19:17 +0000

    Added new module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,4 @@
 rootProject.name = "IdeaPlugin"
 
-//includeBuild("code-to-uml") {
-//    dependencySubstitution {
-//        substitute(module("org.tera201:code-to-uml"))
-//            .using(project(":"))
-//    }
-//}
-
-
 includeBuild("code-to-uml")
+includeBuild("swrminer")
