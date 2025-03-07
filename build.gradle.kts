18634c1cfbcc35a1258bbdaae76cb0b14013e96e
rnaryshkin <r.naryshkin99@gmail.com>
2025-03-07 09:51:56 +0000

    Updated config

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ java {
 }
 
 group = "org.tera201"
-version = "1.6.1"
+version = "1.7.0"
 
 val javafxModules = listOf("javafx-controls", "javafx-graphics", "javafx-swing", "javafx-base")
 val javaFXVersion = "21";
